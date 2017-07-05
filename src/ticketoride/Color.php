1fe49fdceff55a19c18488f836b83324defab48c
Daniela Bento <daniela.bento@olx.com>
2017-07-05 11:01:52 +0100

    Add Color class

---
+++ src/ticketoride/Color.php
@@ -0,0 +1,14 @@
+<?php
+namespace ticketoride;
+
+class Color {
+    const PURPLE = "purple";
+    const WHITE  = "white";
+    const BLUE   = "blue";
+    const YELLOW = "yellow";
+    const ORANGE = "orange";
+    const BLACK  = "black";
+    const RED    = "red";
+    const GREEN  = "green";
+    const GRAY   = "gray";
+}
---
+++ tests/ticketoride/PointsTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dfbento
+ * Date: 7/5/17
+ * Time: 10:54 AM
+ */
+
+namespace ticketoride;
+
+
+class PointsTest
+{
+
+}
