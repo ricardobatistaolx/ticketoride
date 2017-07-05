6cab7077c431aa621b9b150a66703d4f83d44a33
Ricardo Batista <ricardo.batista@olx.com>
2017-07-05 11:44:21 +0100

    Colors work!

--- src/ticketoride/Color.php
+++ src/ticketoride/Color.php
@@ -1,7 +1,8 @@
 <?php
 namespace ticketoride;
 
-class Color {
+class Color
+{
     const PURPLE = "purple";
     const WHITE  = "white";
     const BLUE   = "blue";
@@ -10,4 +11,21 @@ class Color {
     const BLACK  = "black";
     const RED    = "red";
     const GREEN  = "green";
+
+    private $color;
+
+    public function __construct(string $color)
+    {
+        $this->color = $color;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function __toString()
+    {
+        return $this->color;
+    }
 }
--- src/ticketoride/RouteColor.php
+++ src/ticketoride/RouteColor.php
@@ -6,10 +6,5 @@ class RouteColor extends Color
 {
     const GRAY = "gray";
 
-    private $color;
 
-    public function __construct(string $color)
-    {
-        $this->color = $color;
-    }
 }
